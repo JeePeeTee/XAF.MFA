9008ecc930364791fbed7caf1a854d9800fcaa06
JeePee <jpt@sultancrm.nl>
2025-09-04 13:00:16 +0200

    Add Guard Code functionality for Blazor module
    - Introduced a `PopupWindowShowAction` to handle Guard Code entry during logon.
    - Added `GuardCodeEntry` class with logic for code generation, validation, and timeout.
    - Integrated Guard Code logic into Blazor module initialization and user logon process.

--- XAF.MFA/XAF.MFA.Blazor.Server/BlazorModule.cs
+++ XAF.MFA/XAF.MFA.Blazor.Server/BlazorModule.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using DevExpress.CodeParser.Diagnostics;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.DC;
 using DevExpress.ExpressApp.Model;
@@ -8,13 +9,26 @@ using DevExpress.ExpressApp.Updating;
 using DevExpress.ExpressApp.Model.Core;
 using DevExpress.ExpressApp.Model.DomainLogics;
 using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Base.Security;
 using DevExpress.Persistent.BaseImpl;
+using XAF.MFA.Module.BusinessObjects;
+using XAF.MFA.Module.Controllers.Models;
 
 namespace XAF.MFA.Blazor.Server;
 
 [ToolboxItemFilter("Xaf.Platform.Blazor")]
 // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.ModuleBase.
 public sealed class MFABlazorModule : ModuleBase {
+    
+    private const string ShouldEnterGuardCodeKeyName = "user should enter GuardCode on logon";
+
+    private readonly PopupWindowShowAction _requestGuardCode;
+    private uint _guardCodeRetries = 0;
+    private bool _isChangePasswordOnLogonExecuted = false;
+    
     //void Application_CreateCustomModelDifferenceStore(object sender, CreateCustomModelDifferenceStoreEventArgs e) {
     //    e.Store = new ModelDifferenceDbStore((XafApplication)sender, typeof(ModelDifference), true, "Blazor");
     //    e.Handled = true;
@@ -24,7 +38,86 @@ public sealed class MFABlazorModule : ModuleBase {
         e.Handled = true;
     }
     public MFABlazorModule() {
+        _requestGuardCode = new PopupWindowShowAction(null, "EnterGuardCode", "");
+        _requestGuardCode.Execute += new PopupWindowShowActionExecuteEventHandler(enterGuardCodeOnLogon_OnExecute);
+        _requestGuardCode.Cancel += new EventHandler(enterGuardCodeOnLogon_OnCancel);
+        _requestGuardCode.CustomizePopupWindowParams += new CustomizePopupWindowParamsEventHandler(enterGuardCodeOnLogon_OnCustomizePopupWindowParams);
+
     }
+
+    private void enterGuardCodeOnLogon_OnCustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs args) {
+        var objectSpace = Application.CreateObjectSpace(typeof(GuardCodeEntry));
+        args.View = _requestGuardCode.Application.CreateDetailView(objectSpace, new GuardCodeEntry());
+        args.View.Closing += View_Closing;
+        ((DetailView)args.View).ViewEditMode = ViewEditMode.Edit;
+        args.Context = TemplateContext.PopupWindow;
+        var applicationType = Application.GetType();
+        if (applicationType is null) return;
+
+        while (applicationType != typeof(object)) {
+            if (applicationType.Name == "WebApplication" || applicationType.Name == "BlazorApplication") {
+                args.DialogController.CancelAction.Active["NoCancelInChangePasswordView"] = false;
+                break;
+            }
+
+            applicationType = applicationType.BaseType;
+        }
+
+    }
+
+    private void View_Closing(object sender, EventArgs e) {
+        if (!_isChangePasswordOnLogonExecuted) {
+            _requestGuardCode.Application.Exit();
+        }
+    }
+
+    private void enterGuardCodeOnLogon_OnCancel(object sender, EventArgs e) {
+        _requestGuardCode.Application.Exit();
+        _isChangePasswordOnLogonExecuted = true;
+    }
+
+    private void enterGuardCodeOnLogon_OnExecute(object sender, PopupWindowShowActionExecuteEventArgs args) {
+        Guard.ArgumentNotNull(args.PopupWindow, "args.PopupWindow");
+        Guard.ArgumentNotNull(args.PopupWindow, "args.PopupWindow.View");
+        ChangePasswordOnLogon((GuardCodeEntry)args.PopupWindow.View.CurrentObject);
+        _isChangePasswordOnLogonExecuted = true;
+        _requestGuardCode.Active.SetItemValue(ShouldEnterGuardCodeKeyName, false);
+
+    }
+    
+    private void ChangePasswordOnLogon(GuardCodeEntry guardCodeParameters) {
+        Guard.ArgumentNotNull(guardCodeParameters, "guardCodeParameters");
+        Guard.ArgumentNotNull(Application, "Application");
+        Guard.ArgumentNotNull(SecuritySystem.CurrentUser, "SecuritySystem.CurrentUser");
+
+        using var objectSpace = Application.CreateObjectSpace(SecuritySystem.CurrentUser.GetType());
+        var user = (ApplicationUser)objectSpace.GetObject(SecuritySystem.CurrentUser);
+        if (user == null) {
+            var exception = new InvalidOperationException(SecurityExceptionLocalizer.GetExceptionMessage(SecurityExceptionId.UnableToReadCurrentUserData,
+                ((IAuthenticationStandardUser)SecuritySystem.CurrentUser).UserName));
+            if (!Tracing.LogSensitiveData && !LogSensitiveDataSettings.ChangePasswordOnLogonException) {
+                object securedRepresentation;
+                var id = TypeInfo.TryGetSecuredRepresentation(SecuritySystem.CurrentUser, Application?.TypesInfo,
+                    out securedRepresentation)
+                    ? securedRepresentation.ToString()
+                    : Tracing.SensitiveDataReplacement;
+                exception.Data[Tracing.TracingExceptionMessage] =
+                    SecurityExceptionLocalizer.GetExceptionMessage(SecurityExceptionId.UnableToReadCurrentUserData, id);
+            }
+
+            throw exception;
+        }
+
+        if (guardCodeParameters.GuardCodeCorrect) return;
+
+        _guardCodeRetries++;
+        if (_guardCodeRetries < 3)
+            throw new UserFriendlyException("Guardcode is invalid. Try again!");
+
+        _requestGuardCode.Application.LogOff();
+    }
+
+
     public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
         return ModuleUpdater.EmptyModuleUpdaters;
     }
@@ -35,4 +128,26 @@ public sealed class MFABlazorModule : ModuleBase {
         //application.CreateCustomModelDifferenceStore += Application_CreateCustomModelDifferenceStore;
         application.CreateCustomUserModelDifferenceStore += Application_CreateCustomUserModelDifferenceStore;
     }
+    
+    public override IList<PopupWindowShowAction> GetStartupActions() {
+        var user = SecuritySystem.CurrentUser as ApplicationUser;
+
+        // if (user is not { MfaEnabled: true }) return base.GetStartupActions();
+
+        _requestGuardCode.Active.SetItemValue("Request GuardCode is supported by security", IsSupportGuardCode());
+        var securityStrategyBase = Application?.Security;
+        var standardUser = securityStrategyBase?.User as ApplicationUser;
+        var doEnterGuardCodeOnLogon = standardUser != null; // && standardUser.MfaEnabled;
+        _requestGuardCode.Active.SetItemValue(ShouldEnterGuardCodeKeyName, doEnterGuardCodeOnLogon);
+        return [_requestGuardCode];
+    }
+    
+    private bool IsSupportGuardCode() {
+        return true;
+
+        // var supportChangePasswordOption = Application?.Security as ISupportChangePasswordOption;
+        // return supportChangePasswordOption != null && supportChangePasswordOption.IsSupportChangePassword;
+    }
+
+
 }
---
+++ XAF.MFA/XAF.MFA.Module/Controllers/Models/GuardCodeEntry.cs
@@ -0,0 +1,103 @@
+﻿#region MIT License
+
+// ==========================================================
+// 
+// SCRM project - Copyright (c) 2024 JeePeeTee
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// 
+// ===========================================================
+
+#endregion
+
+#region usings
+
+using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Xpo;
+using XAF.MFA.Module.BusinessObjects;
+
+#endregion
+
+namespace XAF.MFA.Module.Controllers.Models;
+
+[DomainComponent]
+public class GuardCodeEntry : INotifyPropertyChanged {
+    private string _guardCode;
+    private string _hiddenGuardCode;
+
+    public GuardCodeEntry() {
+        HiddenGuardCode = GenerateRandomCode();
+        GuardCodeTimeout = DateTime.Now.AddMinutes(10);
+        EmailUser(HiddenGuardCode);
+    }
+
+    public string GuardCode {
+        get => this._guardCode;
+        set {
+            this._guardCode = value;
+            this.RaisePropertyChanged(nameof(GuardCode));
+        }
+    }
+
+    [Browsable(false)]
+    public DateTime GuardCodeTimeout { get; }
+
+    [Browsable(false)]
+    public bool GuardCodeCorrect => this.GuardCode?.ToUpper() == this.HiddenGuardCode && DateTime.Now < this.GuardCodeTimeout;
+
+    [Browsable(false)]
+    public string HiddenGuardCode {
+        get => this._hiddenGuardCode;
+        set {
+            this._hiddenGuardCode = value;
+            this.RaisePropertyChanged(nameof(HiddenGuardCode));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void RaisePropertyChanged(string propertyName) {
+        if (this.PropertyChanged == null)
+            return;
+        this.PropertyChanged((object)this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private static string GenerateRandomCode() {
+        // Define the allowed characters (letters and digits, excluding 'I', '1', 'O', '0')
+        const string allowedChars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+        var rand = new Random();
+        const int length = 6;
+
+        var randomString = new char[length];
+        for (var i = 0; i < length; i++) {
+            randomString[i] = allowedChars[rand.Next(allowedChars.Length)];
+        }
+
+        return new string(randomString);
+    }
+
+    private static void EmailUser(string guardCode) {
+        var currentUser = (ApplicationUser)SecuritySystem.CurrentUser;
+        var ios = XPObjectSpace.FindObjectSpaceByObject(currentUser);
+
+        // Email guardCode to currentUser here...
+    }
+}
